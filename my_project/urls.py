70428e92fb16e941ad593722014b27400df9c876
ididit374-lgtm <Ididit374@gmail.com>
2026-02-07 19:40:14 +0000

    Created a new app called about and added it to installed apps in settings.py

--- my_project/urls.py
+++ my_project/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from hello_world import views as index_views
+
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello, world!")
 
 urlpatterns = [
+    path('', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
