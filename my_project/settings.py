238de6737e8db1474612a6851c47b27f34efddea
ididit374-lgtm <Ididit374@gmail.com>
2026-02-08 23:32:09 +0000

    Settings file and requirement files modified for heroku deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9e7kjwkydq(8orz!)kh3u1n!v1j$g309r+7+7vsri9#im97f-d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
