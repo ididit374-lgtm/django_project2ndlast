571479d152ff0716c81cf0d223c300de3399d4d5
ididit374-lgtm <Ididit374@gmail.com>
2026-02-08 19:39:47 +0000

    Added About app to project and created URL for about page

--- my_project/urls.py
+++ my_project/urls.py
@@ -17,13 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
-
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Hello, world!")
+from about import views as about_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('about/', about_views.about_me, name='about'),
+    path('hello/', index_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
